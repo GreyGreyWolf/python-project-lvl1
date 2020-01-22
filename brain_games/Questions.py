8e8d612a44e45d3d646d29249ff8c64b1faa3fea
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-01-22 20:31:37 +0300

    New Vers

---
+++ brain_games/Questions.py
@@ -0,0 +1,32 @@
+import random, prompt
+from sys import exit
+
+def salute():
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if number even otherwise answer "no"')
+    global name
+    name = prompt.string('\n\nMay I have your name? ')
+    print('Hello, ' + name + ' !')
+
+def qst():
+    i = 0
+    while i < 3:
+        num =  random.randint(1,50)
+        print('\nQuestion: ', num)
+        ans = prompt.string('\nYour answer: ')
+        if  (ans.lower() == 'yes' and num % 2 == 0) or (ans.lower() == 'no' and num % 2 != 0):
+            print('Correct!')
+            i += 1
+        elif ans.lower() == 'yes' and num % 2 != 0:
+            print("\n'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again," + name + "!")
+            exit()
+        elif ans.lower() == 'no' and num % 2 == 0:
+            print("\n'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again," + name + "!")
+            exit()
+        elif ans.lower() != 'no' or ans.lower() != 'yes':
+            print('incorrectly typed response. Let\'s try again, '+ name + '!')
+            exit()
+    if i == 3:
+        print('Congratulations, ' + name + '!')
+    else:
+        exit()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,6 @@
+from brain_games.Questions import salute, qst
+
+
+if __name__ == '__main__':
+        salute()
+        qst()
