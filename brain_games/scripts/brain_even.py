4bcc3c8bab21bcde7b636fb40dedfd8e7009c2ee
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-02-09 20:53:12 +0300

    Final Vers

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,9 @@
 from brain_games import games, engine
 from brain_games.games import Questions
 
-
 def main():
     engine.engine(games.Questions)
 
 
 if __name__ == '__main__':
 	main()
-        
-        
